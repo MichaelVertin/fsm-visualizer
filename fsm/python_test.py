0ac506f0f0dc2ac9e19486a4e4d0a16078260656
Michael Vertin <MichaelVertin>
2025-10-17 19:03:03 -0600

    Fixed Black Formatting Error

--- fsm/python_test.py
+++ fsm/python_test.py
@@ -1,10 +1,6 @@
-
-
 class PythonTest:
     def __init__(self, val):
         self.val = val
 
     def get_val(self):
         return self.val
-
-
--- tests/test_initial.py
+++ tests/test_initial.py
@@ -1,9 +1,8 @@
 import pytest
 from fsm.python_test import PythonTest
 
+
 def test_initial():
     test_val = "Hello, World!"
     my_obj = PythonTest(val=test_val)
     assert my_obj.get_val() == test_val
-
-
